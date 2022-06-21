d1cabacca83b9035a5857f61b821c6a010ce365b
AyanPlugger <ayanpruggertattoo@gmail.com>
2022-06-21 19:03:33 -0300

    commitando

---
+++ app/Views/home.php
@@ -0,0 +1,39 @@
+
+        <!-- inicio do corpo da pagina-->
+        <div class="row">
+            <div class="col">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="https://i.kinja-img.com/gawker-media/image/upload/s--LJImACQb--/c_scale,f_auto,fl_progressive,q_80,w_800/uekooovzksjt1dfg6i24.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item ">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Lada_2107_aka_Lada_Riva_October_1995_1452cc.jpg/1200px-Lada_2107_aka_Lada_Riva_October_1995_1452cc.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item ">
+                    <img src="https://cdn.e-konomista.pt/uploads/2020/08/fiat-multipla.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h3>Texto 1</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1IWXyyaj9BFD20ocBnktNCWgCp1hiEZgzog&usqp=CAU" alt="">
+            </div>
+            <div class="col">
+                <h3>Texto 1</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1IWXyyaj9BFD20ocBnktNCWgCp1hiEZgzog&usqp=CAU" alt="">
+            </div>
+        </div>
+        <!-- fim do corpo da pagina-->
+       
---
+++ app/Views/template/header.php
@@ -0,0 +1,51 @@
+<!-- inicio do header -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?=base_url()?>styles.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+
+    <title>Azov Cars</title>
+</head>
+<body class="fundoCor">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Azov Cars</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="home">Home</a>
+                            <a class="nav-link" href="quemsomos">Quem Somos</a>
+                            <a class="nav-link" href="contato">Contato</a>
+                            <a class="nav-link" href="cadastro">Cadastro</a>
+                            <a class="nav-link" href="Login">Login</a>
+                            <?php
+                                $session = \Config\Services::session(); 
+                                if($session->get('logado')){
+                            ?>
+
+                            <a href="cadastroCarros" class="nav-link">Cadastrar Carro</a>
+                            <a class="nav-link" href="listaCarros">Carros</a>
+                            
+                            <a class="nav-link" href="pessoa">Usuarios</a>
+                            <a class="nav-link" href="sair">Sair</a>
+                            <?php
+                                } 
+                            //fim if
+                            ?>
+                        </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- fim do header -->
